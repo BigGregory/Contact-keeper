7b05af5de5af4e8eeb4b46317e41604026769ec0
Gregory <gregory11031985@gmail.com>
2020-07-13 18:36:26 +0300

    added config, monogDB connection

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => res.json({
-  status: 'success',
-  msg: 'Welcome to Contact Keeper API'
-}))
+// Connect DB
+connectDB();
+
+app.get('/', (req, res) =>
+  res.json({
+    status: 'success',
+    msg: 'Welcome to Contact Keeper API',
+  })
+);
 
 // Define Routes
-app.use('/api/users', require('./routes/users'))
-app.use('/api/contacts', require('./routes/contacts'))
-app.use('/api/auth', require('./routes/auth'))
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 3400;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
