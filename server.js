36c2e512b761cf5155e4e64d4ea4c528caa05817
Gregory <gregory11031985@gmail.com>
2020-07-13 18:16:12 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.json({
+  status: 'success',
+  msg: 'Welcome to Contact Keeper API'
+}))
+
+// Define Routes
+app.use('/api/users', require('./routes/users'))
+app.use('/api/contacts', require('./routes/contacts'))
+app.use('/api/auth', require('./routes/auth'))
+
+const PORT = process.env.PORT || 3400;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
